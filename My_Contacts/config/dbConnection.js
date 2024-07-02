bf17105099df0ca64a7a93229bb282dfbdb28616
GouthamGajam <gouthamgajam@gmail.com>
2024-07-03 00:42:21 +0530

    Done

--- My_Contacts/config/dbConnection.js
+++ My_Contacts/config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        const connect = await mongoose.connect("mongodb+srv://gouthamgajam:Sahiti%402006@gouthamcluster1.ovh9hhz.mongodb.net/mycontacts-backend");
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log("Database connected: ", connect.connection.host, connect.connection.name);
     } catch (err) {
         console.log(err);
