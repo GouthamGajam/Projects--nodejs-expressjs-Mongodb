d08dc9a38147e1ecfd55b202bd4a63782e7cedca
GouthamGajam <gouthamgajam@gmail.com>
2024-07-02 22:45:25 +0530

    Almost done with project

--- My_Contacts/server.js
+++ My_Contacts/server.js
@@ -6,12 +6,13 @@ const dotenv = require("dotenv").config();
 connectDb();
 const app = express();
 
-const port = process.env.PORT || 5009;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
-app.listen(port,()=>{
-    console.log(`Server running in PORT ${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
